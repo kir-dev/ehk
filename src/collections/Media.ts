4ce1739b95f197224995f1335d2eba5436a14ddc
Lipták Péter <liptak.peti.98@gmail.com>
2025-06-22 17:38:38 +0200

    Add labels for Media and Users collections in Payload CMS

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -2,6 +2,10 @@ import type { CollectionConfig } from 'payload'
 
 export const Media: CollectionConfig = {
   slug: 'media',
+    labels: {
+        singular: 'Fájl',
+        plural: 'Fájlok',
+    },
   access: {
     read: () => true,
   },

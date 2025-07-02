083c30c9bb54c4388ce070bb6feb6e071ddf44c3
Lipták Péter <liptak.peti.98@gmail.com>
2025-07-02 19:10:36 +0200

    Refactor Footer component and update page metadata

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -3,7 +3,7 @@ import NewsSection from "@/components/NewsSection";
 export default function Home() {
   return (
       <div className="bg-transparent min-h-screen">
-          <NewsSection />
+          {/*<NewsSection />*/}
       </div>
   );
 }

d60486cee61db655c8fe5aa67e92eddfd4933031
DankaMarci <marciemail7@gmail.com>
2025-08-10 12:24:05 +0200

    Add S3 storage adapter and update dependencies in configuration

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,4 +1,4 @@
-// storage-adapter-import-placeholder
+import { s3Storage } from '@payloadcms/storage-s3'
 import { postgresAdapter } from '@payloadcms/db-postgres'
 import { payloadCloudPlugin } from '@payloadcms/payload-cloud'
 import { lexicalEditor } from '@payloadcms/richtext-lexical'
@@ -55,6 +55,21 @@ export default buildConfig({
   sharp,
   plugins: [
     payloadCloudPlugin(),
-    // storage-adapter-placeholder
+      s3Storage({
+          collections: {
+              media: true
+          },
+          bucket: process.env.S3_BUCKET!,
+          config: {
+              credentials: {
+                  accessKeyId: process.env.S3_ACCESS_KEY_ID!,
+                  secretAccessKey: process.env.S3_SECRET_ACCESS_KEY!,
+              },
+              forcePathStyle: true,
+              region: process.env.S3_REGION,
+              endpoint: process.env.S3_ENDPOINT,
+          },
+          clientUploads: true,
+      }),
   ],
 })

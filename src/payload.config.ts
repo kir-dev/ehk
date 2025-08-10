ccb59c7c7ddf549031a6a17697b6dee178090a7a
DankaMarci <marciemail7@gmail.com>
2025-08-10 14:11:04 +0200

    Add migration for creating users_sessions table and update build script

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -48,7 +48,7 @@ export default buildConfig({
   db: postgresAdapter({
     push: false,
     pool: {
-      connectionString: process.env.DATABASE_URI || '',
+      connectionString: process.env.DATABASE_URI ?? process.env.DATABASE_URL ?? '',
     },
     migrationDir: './src/migrations',
   }),

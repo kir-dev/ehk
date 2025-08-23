924bfc5ddd77a3260e5faf5c88530f2b480d450a
Lipták Péter <liptak.peti.98@gmail.com>
2025-08-23 17:37:16 +0200

    Add Decisions Archive component for displaying legal resolutions

---
+++ src/app/(app)/hatarozatok-tara/page.tsx
@@ -0,0 +1,9 @@
+import { DecisionsArchive } from "@/components/decisions-archive"
+
+export default function DecisionsPage() {
+    return (
+        <div className="min-h-screen bg-gray-50">
+            <DecisionsArchive />
+        </div>
+    )
+}

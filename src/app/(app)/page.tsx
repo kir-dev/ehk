cb9afc919b853988a625711d6cfdfbfa707ba4e6
FearsomeRover <bujgergo@gmail.com>
2025-08-23 13:10:56 +0200

    fixed highlighted days

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -4,6 +4,7 @@ import { getHeroImages } from "@/lib/getHeroImages";
 import MUSZAKSection from "@/components/MUSZAKSection";
 import { getEvents } from "@/lib/payload-cms";
 import Calendar from "@/components/Calendar";
+import Link from "next/link";
 
 export default async function Home() {
   const heroImages = await getHeroImages();
@@ -19,8 +20,30 @@ export default async function Home() {
         </section>
 
         <section className="py-16 px-4">
-          <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-5 gap-8">
-            <Calendar events={events} className="col-span-1" />
+          <div className="grid grid-cols-1 md:grid-cols-5 gap-y-8 md:gap-x-8 items-center mx-8">
+            <div className="col-span-1 flex justify-between flex-col gap-y-4 h-full">
+              <div className="bg-white rounded-lg shadow-md overflow-hidden flex flex-col relative p-4 flex-1">
+                Fontos linkek:
+                <ul className="list-disc list-inside">
+                  <li>
+                    <Link href="https://neptun.bme.hu" target="_blank">
+                      Neptun
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="https://mueper.bme.hu" target="_blank">
+                      Műeper
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="https://kefir.bme.hu" target="_blank">
+                      Kefír
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+              <Calendar events={events} className="w-full h-2/3" />
+            </div>
             <div className="max-w-7xl mx-auto col-span-1 sm:col-span-3">
               <h2 className="text-3xl font-bold mb-8 border-b-2 border-red-500 pb-2 inline-block">
                 Hírek

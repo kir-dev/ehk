325a2cdc9f7aa28e323980d7e57529f2896646d5
Lipták Péter <liptak.peti.98@gmail.com>
2025-08-24 21:08:11 +0200

    Add event migration and update payload types for events

--- src/payload-types.ts
+++ src/payload-types.ts
@@ -13,53 +13,53 @@
  * via the `definition` "supportedTimezones".
  */
 export type SupportedTimezones =
-  | "Pacific/Midway"
-  | "Pacific/Niue"
-  | "Pacific/Honolulu"
-  | "Pacific/Rarotonga"
-  | "America/Anchorage"
-  | "Pacific/Gambier"
-  | "America/Los_Angeles"
-  | "America/Tijuana"
-  | "America/Denver"
-  | "America/Phoenix"
-  | "America/Chicago"
-  | "America/Guatemala"
-  | "America/New_York"
-  | "America/Bogota"
-  | "America/Caracas"
-  | "America/Santiago"
-  | "America/Buenos_Aires"
-  | "America/Sao_Paulo"
-  | "Atlantic/South_Georgia"
-  | "Atlantic/Azores"
-  | "Atlantic/Cape_Verde"
-  | "Europe/London"
-  | "Europe/Berlin"
-  | "Africa/Lagos"
-  | "Europe/Athens"
-  | "Africa/Cairo"
-  | "Europe/Moscow"
-  | "Asia/Riyadh"
-  | "Asia/Dubai"
-  | "Asia/Baku"
-  | "Asia/Karachi"
-  | "Asia/Tashkent"
-  | "Asia/Calcutta"
-  | "Asia/Dhaka"
-  | "Asia/Almaty"
-  | "Asia/Jakarta"
-  | "Asia/Bangkok"
-  | "Asia/Shanghai"
-  | "Asia/Singapore"
-  | "Asia/Tokyo"
-  | "Asia/Seoul"
-  | "Australia/Brisbane"
-  | "Australia/Sydney"
-  | "Pacific/Guam"
-  | "Pacific/Noumea"
-  | "Pacific/Auckland"
-  | "Pacific/Fiji";
+  | 'Pacific/Midway'
+  | 'Pacific/Niue'
+  | 'Pacific/Honolulu'
+  | 'Pacific/Rarotonga'
+  | 'America/Anchorage'
+  | 'Pacific/Gambier'
+  | 'America/Los_Angeles'
+  | 'America/Tijuana'
+  | 'America/Denver'
+  | 'America/Phoenix'
+  | 'America/Chicago'
+  | 'America/Guatemala'
+  | 'America/New_York'
+  | 'America/Bogota'
+  | 'America/Caracas'
+  | 'America/Santiago'
+  | 'America/Buenos_Aires'
+  | 'America/Sao_Paulo'
+  | 'Atlantic/South_Georgia'
+  | 'Atlantic/Azores'
+  | 'Atlantic/Cape_Verde'
+  | 'Europe/London'
+  | 'Europe/Berlin'
+  | 'Africa/Lagos'
+  | 'Europe/Athens'
+  | 'Africa/Cairo'
+  | 'Europe/Moscow'
+  | 'Asia/Riyadh'
+  | 'Asia/Dubai'
+  | 'Asia/Baku'
+  | 'Asia/Karachi'
+  | 'Asia/Tashkent'
+  | 'Asia/Calcutta'
+  | 'Asia/Dhaka'
+  | 'Asia/Almaty'
+  | 'Asia/Jakarta'
+  | 'Asia/Bangkok'
+  | 'Asia/Shanghai'
+  | 'Asia/Singapore'
+  | 'Asia/Tokyo'
+  | 'Asia/Seoul'
+  | 'Australia/Brisbane'
+  | 'Australia/Sydney'
+  | 'Pacific/Guam'
+  | 'Pacific/Noumea'
+  | 'Pacific/Auckland'
+  | 'Pacific/Fiji';
 
 export interface Config {
   auth: {
@@ -72,12 +72,12 @@ export interface Config {
     representatives: Representative;
     reminders: Reminder;
     news: News;
-    "hero-images": HeroImage;
-    "muszak-paper": MuszakPaper;
+    'hero-images': HeroImage;
+    'muszak-paper': MuszakPaper;
     events: Event;
-    "payload-locked-documents": PayloadLockedDocument;
-    "payload-preferences": PayloadPreference;
-    "payload-migrations": PayloadMigration;
+    'payload-locked-documents': PayloadLockedDocument;
+    'payload-preferences': PayloadPreference;
+    'payload-migrations': PayloadMigration;
   };
   collectionsJoins: {};
   collectionsSelect: {
@@ -86,18 +86,12 @@ export interface Config {
     representatives: RepresentativesSelect<false> | RepresentativesSelect<true>;
     reminders: RemindersSelect<false> | RemindersSelect<true>;
     news: NewsSelect<false> | NewsSelect<true>;
-    "hero-images": HeroImagesSelect<false> | HeroImagesSelect<true>;
-    "muszak-paper": MuszakPaperSelect<false> | MuszakPaperSelect<true>;
+    'hero-images': HeroImagesSelect<false> | HeroImagesSelect<true>;
+    'muszak-paper': MuszakPaperSelect<false> | MuszakPaperSelect<true>;
     events: EventsSelect<false> | EventsSelect<true>;
-    "payload-locked-documents":
-      | PayloadLockedDocumentsSelect<false>
-      | PayloadLockedDocumentsSelect<true>;
-    "payload-preferences":
-      | PayloadPreferencesSelect<false>
-      | PayloadPreferencesSelect<true>;
-    "payload-migrations":
-      | PayloadMigrationsSelect<false>
-      | PayloadMigrationsSelect<true>;
+    'payload-locked-documents': PayloadLockedDocumentsSelect<false> | PayloadLockedDocumentsSelect<true>;
+    'payload-preferences': PayloadPreferencesSelect<false> | PayloadPreferencesSelect<true>;
+    'payload-migrations': PayloadMigrationsSelect<false> | PayloadMigrationsSelect<true>;
   };
   db: {
     defaultIDType: number;
@@ -106,7 +100,7 @@ export interface Config {
   globalsSelect: {};
   locale: null;
   user: User & {
-    collection: "users";
+    collection: 'users';
   };
   jobs: {
     tasks: unknown;
@@ -198,6 +192,7 @@ export interface Representative {
         id?: string | null;
       }[]
     | null;
+  picture?: (number | null) | Media;
   introduction: {
     text_hu: {
       root: {
@@ -207,8 +202,8 @@ export interface Representative {
           version: number;
           [k: string]: unknown;
         }[];
-        direction: ("ltr" | "rtl") | null;
-        format: "left" | "start" | "center" | "right" | "end" | "justify" | "";
+        direction: ('ltr' | 'rtl') | null;
+        format: 'left' | 'start' | 'center' | 'right' | 'end' | 'justify' | '';
         indent: number;
         version: number;
       };
@@ -222,17 +217,15 @@ export interface Representative {
           version: number;
           [k: string]: unknown;
         }[];
-        direction: ("ltr" | "rtl") | null;
-        format: "left" | "start" | "center" | "right" | "end" | "justify" | "";
+        direction: ('ltr' | 'rtl') | null;
+        format: 'left' | 'start' | 'center' | 'right' | 'end' | 'justify' | '';
         indent: number;
         version: number;
       };
       [k: string]: unknown;
     };
   };
-  faculty?:
-    | ("ÉMK" | "GPK" | "ÉPK" | "VBK" | "VIK" | "GTK" | "TTK" | "KJK")
-    | null;
+  faculty?: ('ÉMK' | 'GPK' | 'ÉPK' | 'VBK' | 'VIK' | 'GTK' | 'TTK' | 'KJK') | null;
   order?: number | null;
   files?:
     | {
@@ -256,7 +249,7 @@ export interface Reminder {
   date: string;
   displayText: string;
   file: number | Media;
-  type: "EHK" | "EHDK";
+  type: 'EHK' | 'EHDK';
   updatedAt: string;
   createdAt: string;
 }
@@ -283,8 +276,8 @@ export interface News {
           version: number;
           [k: string]: unknown;
         }[];
-        direction: ("ltr" | "rtl") | null;
-        format: "left" | "start" | "center" | "right" | "end" | "justify" | "";
+        direction: ('ltr' | 'rtl') | null;
+        format: 'left' | 'start' | 'center' | 'right' | 'end' | 'justify' | '';
         indent: number;
         version: number;
       };
@@ -298,8 +291,8 @@ export interface News {
           version: number;
           [k: string]: unknown;
         }[];
-        direction: ("ltr" | "rtl") | null;
-        format: "left" | "start" | "center" | "right" | "end" | "justify" | "";
+        direction: ('ltr' | 'rtl') | null;
+        format: 'left' | 'start' | 'center' | 'right' | 'end' | 'justify' | '';
         indent: number;
         version: number;
       };
@@ -308,19 +301,19 @@ export interface News {
   };
   date: string;
   tags: (
-    | "EHK"
-    | "Oktatás"
-    | "Juttatás"
-    | "Kollégium"
-    | "Pályázat"
-    | "Sport"
-    | "Külügy"
-    | "Rendezvények"
-    | "Közélet"
-    | "Felhívás"
-    | "Beszámoló"
-    | "Tájékoztatás"
-    | "Kiemelt hír"
+    | 'EHK'
+    | 'Oktatás'
+    | 'Juttatás'
+    | 'Kollégium'
+    | 'Pályázat'
+    | 'Sport'
+    | 'Külügy'
+    | 'Rendezvények'
+    | 'Közélet'
+    | 'Felhívás'
+    | 'Beszámoló'
+    | 'Tájékoztatás'
+    | 'Kiemelt hír'
   )[];
   updatedAt: string;
   createdAt: string;
@@ -386,40 +379,40 @@ export interface PayloadLockedDocument {
   id: number;
   document?:
     | ({
-        relationTo: "users";
+        relationTo: 'users';
         value: number | User;
       } | null)
     | ({
-        relationTo: "media";
+        relationTo: 'media';
         value: number | Media;
       } | null)
     | ({
-        relationTo: "representatives";
+        relationTo: 'representatives';
         value: number | Representative;
       } | null)
     | ({
-        relationTo: "reminders";
+        relationTo: 'reminders';
         value: number | Reminder;
       } | null)
     | ({
-        relationTo: "news";
+        relationTo: 'news';
         value: number | News;
       } | null)
     | ({
-        relationTo: "hero-images";
+        relationTo: 'hero-images';
         value: number | HeroImage;
       } | null)
     | ({
-        relationTo: "muszak-paper";
+        relationTo: 'muszak-paper';
         value: number | MuszakPaper;
       } | null)
     | ({
-        relationTo: "events";
+        relationTo: 'events';
         value: number | Event;
       } | null);
   globalSlug?: string | null;
   user: {
-    relationTo: "users";
+    relationTo: 'users';
     value: number | User;
   };
   updatedAt: string;
@@ -432,7 +425,7 @@ export interface PayloadLockedDocument {
 export interface PayloadPreference {
   id: number;
   user: {
-    relationTo: "users";
+    relationTo: 'users';
     value: number | User;
   };
   key?: string | null;
@@ -518,6 +511,7 @@ export interface RepresentativesSelect<T extends boolean = true> {
         email?: T;
         id?: T;
       };
+  picture?: T;
   introduction?:
     | T
     | {
@@ -658,6 +652,7 @@ export interface Auth {
   [k: string]: unknown;
 }
 
-declare module "payload" {
+
+declare module 'payload' {
   export interface GeneratedTypes extends Config {}
 }

b2e4b94e1609110bbd1cc2707dfacbe5b3a40e13
GlizzyHacker <saxattila@gmail.com>
2025-10-13 20:03:17 +0200

    english translation

--- src/components/language-education-content.tsx
+++ src/components/language-education-content.tsx
@@ -16,26 +16,39 @@ export default function LanguageEducationContent() {
               <h3 className="font-bold text-xl leading-tight text-gray-900">
                 {t(
                   "Minden, ami nyelv, egy helyen!",
-                  "No regulations available at the moment."
+                  "Everything about language, all in one place!"
                 )}
               </h3>
               <p className="text-gray-700 text-lg richtext">
                 {t(
                   `Az Idegen Nyelvi Központ 6 nyelven, több szinten kínál általános,
-              szaknyelvi és vizsgafelkészítő kurzusokat. Itt kredites és 0-kredites
-              kurzusok felvétére is van lehetőséged!
-              Ha nyelvvizsgázni szeretnél, lehetőséged van itt helyben, a BME-n is
-              nyelvvizsgát letenned!
-              Ha már mindez megvan, de még mindig fejlődni szeretnél, akkor
-              választhatsz a Központ fordító és tolmács szakirányú továbbképzései
-              közül, ami újabb munkalehetőséget is ad.
-              A részleteket megtalálod itt: `,
-                  "No regulations available at the moment."
+                  szaknyelvi és vizsgafelkészítő kurzusokat. Itt kredites és 0-kredites
+                  kurzusok felvétére is van lehetőséged!
+                  Ha nyelvvizsgázni szeretnél, lehetőséged van itt helyben, a BME-n is
+                  nyelvvizsgát letenned!
+                  Ha már mindez megvan, de még mindig fejlődni szeretnél, akkor
+                  választhatsz a Központ fordító és tolmács szakirányú továbbképzései
+                  közül, ami újabb munkalehetőséget is ad.
+                  A részleteket megtalálod itt:`,
+                  `The Centre for Modern Languages offers general, 
+                  professional and examination preparation courses in six foreign languages and at several proficiency levels. 
+                  Both credit-bearing and non-credit courses are available for students. 
+                  If you wish to take a language examination, you have the opportunity to do so right here at BME. 
+                  If you already hold a language certificate but would like to continue developing your language skills, 
+                  you may also choose from the Centre’s postgraduate specialist training programmes in translation and interpreting, 
+                  which open up further professional opportunities. Further details are available here:`
                 )}
-                <a href="http://www.inyk.bme.hu/">www.inyk.bme.hu</a>
+                &nbsp;
+                <a
+                  href="http://www.inyk.bme.hu/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  www.inyk.bme.hu
+                </a>
               </p>
             </div>
-            <div className="">
+            <div>
               <Image
                 src={"/nyelviskola.png"}
                 alt={"Nyelviskola"}

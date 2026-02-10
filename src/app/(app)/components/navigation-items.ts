097b3c49fcd1bea395acaaa921d3961c6961062f
GlizzyHacker <saxattila@gmail.com>
2026-02-10 17:59:40 +0100

    fixed navigation link

--- src/app/(app)/components/navigation-items.ts
+++ src/app/(app)/components/navigation-items.ts
@@ -105,7 +105,7 @@ export function getNavigationItems(lang: string): NavigationItem[] {
         },
         {
           label: t("EHK ösztöndíjak", "EHK scholarships"),
-          href: "/ehk-osztondij",
+          href: link("/ehk-osztondij"),
           targetBlank: false,
         },
         {
